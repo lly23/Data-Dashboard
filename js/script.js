75b024037e1b6ec27ec56df26318d82a49eb4b84
Linh Ly <lly23@live.unc.edu>
2018-11-08 02:25:23 -0500

    added two charts and moved table to the bottom

--- js/script.js
+++ js/script.js
@@ -1,4 +1,145 @@
 $(function () {
+    // Pie Chart
+    var pieChartURL = '/data/json/BreakdownInTech.json';
+    var pieChartData = [];
+
+    Highcharts.chart('graphOne', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Browser market shares in January, 2018'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'Chrome',
+                y: 61.41,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Internet Explorer',
+                y: 11.84
+            }, {
+                name: 'Firefox',
+                y: 10.85
+            }, {
+                name: 'Edge',
+                y: 4.67
+            }, {
+                name: 'Safari',
+                y: 4.18
+            }, {
+                name: 'Other',
+                y: 7.05
+            }]
+        }]
+    });
+
+    // Reasons for Racial and Gender Gaps Chart
+    var surveyDataURL = '/data/json/GapsPew.json';
+    var surveyData = [];
+    var responses = [];
+    var seriesAsian = [];
+    var seriesBlack = [];
+    var seriesHispanic = [];
+    var seriesWhite = [];
+
+    // First AJAX call for workforce data
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        data: surveyData,
+        url: surveyDataURL,
+        async: true,
+        success: function(surveyData) {
+            console.log(surveyData);
+            
+            for (var i = 0; i < surveyData.length; i++) {
+                // High Charts
+                responses.push(surveyData[i].Responses);
+                seriesWhite.push(surveyData[i].White);
+                seriesAsian.push(surveyData[i].Asian);
+                seriesHispanic.push(surveyData[i].Hispanic);
+                seriesBlack.push(surveyData[i].Black);
+            }
+
+            Highcharts.chart('graphTwo', {
+                chart: {
+                    type: 'column'
+                },
+            
+                title: {
+                    text: 'Responses to the Racial Gaps among STEM employees'
+                },
+            
+                xAxis: {
+                    categories: responses
+                },
+            
+                yAxis: {
+                    allowDecimals: false,
+                    min: 0,
+                    max: 100,
+                    title: {
+                        text: 'Percentage of Employees'
+                    }
+                },
+            
+                tooltip: {
+                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                    pointFormat: '<tr><td style="color:"#000";padding:0">{series.name}: </td>' +
+                        '<td style="padding:0"><b>{point.y}%</b></td></tr>',
+                    footerFormat: '</table>',
+                    shared: true,
+                    useHTML: true
+                },
+            
+                plotOptions: {
+                    column: {
+                        pointPadding: 0.2,
+                        borderWidth: 0
+                    }
+                },
+            
+                series: 
+                [{
+                    name: 'White',
+                    data: seriesWhite,
+                    color: '#006287'
+                }, {
+                    name: 'Asian',
+                    data: seriesAsian,
+                    color: '#AFCED8'
+                }, {
+                    name: 'Hispanic',
+                    data: seriesHispanic
+                }, {
+                    name: 'Black',
+                    data: seriesBlack,
+                    color: '#6DA9BC'
+                }]
+            });
+        } //end of success function
+    }); //end of workforce ajax call
+
     //WorkForce Table
     var workforceURL = '/data/txt/AllPercentages.txt';
 
@@ -21,119 +162,5 @@ $(function () {
             className: 'mdl-data-table__cell--non-numeric'
         }] //close columnDefs
     });
-
-    // WorkForce Chart
-    // var workforceURL = '/data/json/AllPercentages.json';
-    // var workforceData = [];
-    // var companies = [];
-    // var seriesFemale = [];
-    // var seriesMale = [];
-    // var seriesAsian = [];
-    // var seriesBlack = [];
-    // var seriesLatino = [];
-    // var seriesWhite = [];
-
-    // First AJAX call for workforce data
-    // $.ajax({
-    //     type: 'GET',
-    //     dataType: 'json',
-    //     data: workforceData,
-    //     url: workforceURL,
-    //     async: true,
-    //     success: function(workforceData) {
-    //         console.log(workforceData);
-            
-    //         for (var i = 0; i < workforceData.length; i++) {
-    //             // High Charts
-    //             companies.push(workforceData[i].Company);
-    //             seriesFemale.push(workforceData[i].Female);
-    //             seriesMale.push(workforceData[i].Male);
-    //             seriesAsian.push(workforceData[i].Asian);
-    //             seriesBlack.push(workforceData[i].Black);
-    //             seriesLatino.push(workforceData[i].Latino);
-    //             seriesWhite.push(workforceData[i].White);
-    //         }
-
-    //         Highcharts.chart('workforce', {
-    //             chart: {
-    //                 type: 'column'
-    //             },
-            
-    //             title: {
-    //                 text: 'Percent of Total Workforce in Tech Industries'
-    //             },
-            
-    //             xAxis: {
-    //                 categories: companies
-    //             },
-            
-    //             yAxis: {
-    //                 allowDecimals: false,
-    //                 min: 0,
-    //                 max: 100,
-    //                 title: {
-    //                     text: 'Percentage of Employees'
-    //                 }
-    //             },
-            
-    //             tooltip: {
-    //                 formatter: function () {
-    //                     return '<b>' + this.x + '</b><br/>' +
-    //                         this.series.name + ': ' + this.y + '<br/>' +
-    //                         'Total: ' + this.point.stackTotal;
-    //                 }
-    //             },
-            
-    //             plotOptions: {
-    //                 column: {
-    //                     stacking: 'normal'
-    //                 }
-    //             },
-            
-    //             series: 
-    //             [{
-    //                 name: 'Male',
-    //                 data: seriesMale,
-    //                 stack: 'gender'
-    //             }, {
-    //                 name: 'Female',
-    //                 data: seriesFemale,
-    //                 stack: 'gender'
-    //             }, {
-    //                 name: 'Asian',
-    //                 data: seriesAsian,
-    //                 stack: 'race'
-    //             }, {
-    //                 name: 'Latino',
-    //                 data: seriesLatino,
-    //                 stack: 'race'
-    //             }, {
-    //                 name: 'Black',
-    //                 data: seriesBlack,
-    //                 stack: 'race'
-    //             }, {
-    //                 name: 'White',
-    //                 data: seriesWhite,
-    //                 stack: 'race'
-    //             }]
-    //         });
-    //     } //end of success function
-    // }); //end of workforce ajax call
-
-    // Women in School Chart
-    // var womenSchoolURL = '/data/json/HighSchoolBachelorsCompletion.json';
-    // var womenSchoolData = [];
-
-    // $.ajax({
-    //     type: 'GET',
-    //     dataType: 'json',
-    //     data: worksSchoolData,
-    //     url: womenSchoolURL,
-    //     async: true,
-    //     success: function(womenSchoolData) {
-
-    //     } //end of success function
-    // }); //end of education ajax call
-
     
 });
